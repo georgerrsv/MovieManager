20c45c09a7f1478a961d1c1370eb32a0afb6d8ee
George <georgerrsilva@alu.ufc.br>
2023-12-06 10:21:04 -0300

    update

---
+++ database/createquery.sql
@@ -0,0 +1,34 @@
+-- CREATING DATABASE --
+
+CREATE DATABASE movieManager;
+
+
+-- CREATING TABLES --
+
+CREATE TABLE movie (
+    movieId SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    director VARCHAR(255) NOT NULL,
+    year INTEGER NOT NULL,
+    duration INTEGER NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    classification VARCHAR(255) NOT NULL,
+    rating INTEGER NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE user (
+    userId SERIAL PRIMARY KEY,
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE review (
+    reviewId SERIAL PRIMARY KEY,
+    movieId INTEGER REFERENCES movie(movieId) ON DELETE CASCADE NOT NULL,
+    userId INTEGER REFERENCES user(userId) ON DELETE CASCADE NOT NULL,
+    rating INTEGER NOT NULL,
+    review TEXT NOT NULL,
+);
