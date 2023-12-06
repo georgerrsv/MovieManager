a3c5298d0097450af7359727386fd2639fc7d649
George <georgerrsilva@alu.ufc.br>
2023-12-06 10:33:03 -0300

    update

---
+++ client/udpClient.py
@@ -0,0 +1,18 @@
+from socket import *
+
+class UDPClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client = socket(AF_INET, SOCK_DGRAM)
+        self.client.settimeout(2)
+
+    def sendRequest(self, obj):
+        self.client.sendto(obj.encode('utf-8'), (self.host, self.port))
+
+    def getResponse(self):
+        data, addr = self.client.recvfrom(1024)
+        return data.decode('utf-8')
+    
+    def close(self):
+        self.client.close()
